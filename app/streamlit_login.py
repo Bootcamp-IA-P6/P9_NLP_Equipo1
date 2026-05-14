a5c83790d811515bd2face5cf32adf140598f74d
Juan Manuel Iriondo Ortega <54706910+jumair@users.noreply.github.com>
2026-05-14 14:42:54 +0200

    feat: Login test completed successfully, #12

---
+++ app/streamlit_login.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+def login_screen():
+    st.header("This app is private.")
+    st.subheader("Please log in.")
+    st.button("Log in with Google", on_click=st.login)
+
+
+# -------------------------
+# Programa
+# -------------------------
+
+if not st.user.is_logged_in:
+    login_screen()
+else:
+    # st.user
+    st.header(f"Welcome, {st.user.name}!")
+
+
+st.button("Log out", on_click=st.logout)
