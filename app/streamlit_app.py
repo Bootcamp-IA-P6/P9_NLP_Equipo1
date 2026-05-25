c5d23877de5c613638b2a5c49fcb537e98ec9242
Juan Manuel Iriondo Ortega <54706910+jumair@users.noreply.github.com>
2026-05-25 09:06:35 +0200

    feat: Cleaning code before testing the model, #12

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -4,6 +4,14 @@ import pandas as pd
 import json
 import sys
 from pathlib import Path
+import ast
+
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 # Prepara el path para poder acceder a los scripts y funciones en carpeta src
 sys.path.append(".")
@@ -12,14 +20,16 @@ from src.hatescan.scraping.youtube_scraper import fetch_comments
 # Import the utility to extract the ID from full URLs
 from src.hatescan.utils.youtube_utils import extract_video_id
 
+from src.hatescan.database.database import save_hatescan_results
+
 
 from supabase import create_client, Client
 # ---------------------------------------------------------------------
 # Supabase
 # ---------------------------------------------------------------------
 # Leer secretos
-SUPABASE_URL = st.secrets.connections.supabase.SUPABASE_URL2
-SUPABASE_KEY = st.secrets.connections.supabase.SUPABASE_KEY2
+SUPABASE_URL = st.secrets.connections.supabase.SUPABASE_URL
+SUPABASE_KEY = st.secrets.connections.supabase.SUPABASE_KEY
 
 # Crear conexión
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
@@ -29,22 +39,47 @@ supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 # Definimos el estilo CSS apuntando al 'key' del contenedor
 css = """
 .st-key-contenedor_login {
-    background-color: rgba(173, 216, 230, 0.5); /* Fondo azul claro */
+    background-color: rgba(138, 218, 242, 0.5); /* Fondo azul claro */
     border: 2px solid #2b5c8f;
     border-radius: 10px;
     padding: 15px;
 }
+
 .st-key-contenedor_personalizado {
-    background-color: rgba(173, 216, 230, 0.3); /* Fondo azul claro */
+    background-color: rgba(138, 218, 242, 0.3); /* Fondo azul claro */
     border: 2px solid #2b5c8f;
     border-radius: 10px;
     padding: 15px;
 }
+
 .st-key-contenedor_url {
-    background-color: rgba(173, 216, 230, 0.1); /* Fondo azul claro */
+    background-color: rgba(138, 218, 242, 0.1); /* Fondo azul claro */
     border: 2px solid #2b5c8f;
     border-radius: 10px;
     padding: 15px;
+}
+
+/* Cambiar el fondo y el color del texto del botón */
+div.stButton > button:first-child {
+    background-color: #F07B3E;
+    color: #ffffff;
+    border-radius: 25px; /* Bordes redondeados */
+    transition: background-color 1s ease !important;
+}
+        
+/* Cambiar el color al pasar el ratón por encima (hover) */
+div.stButton > button:hover {
+    background-color: #00ccff;
+    color: #000000;
+}
+
+/* Colorear metricas */
+div[data-testid="metric-container"] {
+    background-color: rgba(28, 131, 225, 0.1); /* Color de fondo con transparencia */
+    border: 1px solid rgba(28, 131, 225, 0.3); /* Borde opcional */
+    padding: 15px; /* Espaciado interno */
+    border-radius: 10px; /* Bordes redondeados */
+    color: #1E6777; /* Color del texto dentro del recuadro */
 }
 """
 
@@ -52,21 +87,15 @@ css = """
 # Datos de prueba
 # -------------------------
 
-# OJO con los datos recibidos si son true, false o null ¿Darán problemas? ¿Python debería hacerlo sólo . Usar json_load?
-# en formato json para python y streamlit como poner un valor booleano
-# en python como acceder a un campo de json
+# --------------------------------------------
+# Datos para probar las inserciones de datos
+# --------------------------------------------
 
-prueba = '{"nombre": "Juanma", "casado": true}'
+session_user = "coder_test_session" # Hardcodeado
+video_title = "HateScan" # Me lo invento
+# video_id = "dQw4w9WgXcQ" # Se puede sacar de la URL
 
-result = """{ 
-    "video_url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-    "model_used": "transformer_roberta",
-    "total_comments": 150,
-    "toxic_count": 23,
-    "non_toxic_count": 127
-}"""
-
-resultado = """{
+resultado = {
   "video_url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
   "model_used": "transformer_roberta",
   "total_comments": 150,
@@ -74,31 +103,31 @@ resultado = """{
   "non_toxic_count": 127,
   "comments": [
     {
-      "comment_id": "abc123",
+      "comment_id": "pru123",
       "text_original": "you are so stupid and pathetic",
-      "is_toxic": true,
+      "is_toxic": True,
       "confidence": 0.94,
       "categories": {
-        "is_hatespeech": null,
-        "is_racist": null,
-        "is_threat": null,
-        "is_obscene": null
+        "is_hatespeech": None,
+        "is_racist": None,
+        "is_threat": None,
+        "is_obscene": None
       }
     },
     {
-      "comment_id": "def456",
+      "comment_id": "pru456",
       "text_original": "great video, loved it!",
-      "is_toxic": false,
+      "is_toxic": False,
       "confidence": 0.97,
       "categories": {
-        "is_hatespeech": null,
-        "is_racist": null,
-        "is_threat": null,
-        "is_obscene": null
+        "is_hatespeech": None,
+        "is_racist": None,
+        "is_threat": None,
+        "is_obscene": None
       }
     }
   ]
-}"""
+}
 
 # -------------------------
 # Funciones
@@ -126,6 +155,7 @@ def login_screen():
     # Línea divisoria naranja con CSS en st.markdown
     st.markdown("<hr style='border: 2px solid orange;'>", unsafe_allow_html=True)
 
+
 def get_comments(url):
     # Clean the URL to obtain the 11-character video ID
     video_id = extract_video_id(url)
@@ -139,12 +169,29 @@ def get_comments(url):
     # Fetch the comments using the cleaned ID
     return fetch_comments(video_id, max_results=5)
 
-def get_users():
-    # SELECT * FROM users
+
+# def save_comments(datos):
+#     # # 1. Datos externos simulados como cadena de texto
+#     # cadena_externa = '{"usuario": "Ana", "edad": 28, "activo": true}'
+
+#     # # 2. Convertir la cadena a un diccionario de Python
+#     # datos_json = json.loads(cadena_externa)
+
+#     # print(datos_json["usuario"]) # Imprime: Ana
+
+#     comment = {}
+#     comment["model_used"] = datos["model_used"]
+#     comment["text_original"] = datos["comments"][0]["text_original"] 
+#     comment["is_toxic"] = datos["comments"][0]["is_toxic"] 
+
+#     return comment
+
+
+def show_searches():
+    # SELECT * FROM searches
     response = (
         supabase
-        .table("users")
-        #.table("users_test")
+        .table("searches")
         .select("*")
         .execute()
     )
@@ -152,40 +199,144 @@ def get_users():
     # Datos JSON devueltos
     return response.data
 
-def search_user(user):
-    # SELECT * FROM users
+
+def show_comments():
+    # SELECT * FROM comments
     response = (
         supabase
-        .table("users")
-        #.table("users_test")
+        .table("comments")
         .select("*")
-        .eq("email", user)
         .execute()
     )
 
     # Datos JSON devueltos
     return response.data
 
-def save_comments(datos):
-    # # 1. Datos externos simulados como cadena de texto
-    # cadena_externa = '{"usuario": "Ana", "edad": 28, "activo": true}'
+def dashboard_models():
+    st.header("DashBoard")
 
-    # # 2. Convertir la cadena a un diccionario de Python
-    # datos_json = json.loads(cadena_externa)
+    df = pd.read_csv("data/raw/metricas.csv")
+    st.dataframe(df) # Muestra la tabla interactiva
 
-    # print(datos_json["usuario"]) # Imprime: Ana
+    # Convertir columnas en numéricas
+    columnas = ['Duration_sg', 'f1_macro', 'f1_macro_test', 'f1_macro_train']
+    df[columnas] = df[columnas].apply(pd.to_numeric)
 
-    comment = {}
-    comment["model_used"] = datos["model_used"]
-    comment["text_original"] = datos["comments"][0]["text_original"] 
-    comment["is_toxic"] = datos["comments"][0]["is_toxic"] 
+    # print(df.info())
+    
+    # ------------------------------------------------------------
+    # Botones y filtros
+    # ------------------------------------------------------------
 
-    return comment
+    # Separador visual opcional
+    st.markdown("---")
+
+    # Fila 1: Contenedor horizontal con multiselect
+    with st.container(horizontal=True, border=True):
+        st.write("Filtros")
+        seleccion = st.multiselect(
+            "Selecciona tu(s) modelo(s)",
+             options=df['Run Name'].unique(),
+             default=df['Run Name'].unique()
+             )
+
+    # Mostrar resultados seleccionados
+    # st.write("Has seleccionado:", seleccion)
+
+    # Fila 2: Contenedor horizontal con botones
+    with st.container(horizontal=True, border=True):
+        btn_duration = st.button("Duration")
+        btn_f1_macro = st.button("F1_macro")
+        btn_f1_macro_global = st.button("F1_macro_global")
+        btn_f1_macro_test = st.button("F1_macro_test")
+
+    df_filtered = df[(df['Run Name'].isin(seleccion))]
+
+    if btn_duration:
+         # Crear tu gráfico con Plotly
+        fig_duration = px.line(df_filtered, x='Run Name', y='Duration_sg',
+                            title='Duración en segundos',
+                            labels={'Run Name': 'Modelo'})
+        
+        # Poner grosor de línea
+        fig_duration.update_traces(line=dict(width=3))
+        st.plotly_chart(fig_duration)
+
+    if btn_f1_macro:
+        fig_f1_macro = px.bar(df_filtered, x="Run Name", y="f1_macro", title="F1_macro")
+        st.plotly_chart(fig_f1_macro)
+
+    if btn_f1_macro_global:
+        fig_f1_macro_global = px.bar(df_filtered, x="Run Name", y=["f1_macro", "f1_macro_test", "f1_macro_train"], title="F1_macro_global")
+        st.plotly_chart(fig_f1_macro_global, use_container_width=True)
+
+    if btn_f1_macro_test:
+        df_f1_macro_test_sort = df_filtered.sort_values(by="f1_macro_test")
+        fig_f1_macro_test = px.bar(df_f1_macro_test_sort, x="f1_macro_test", y="Run Name", orientation='h', title="F1_macro_test")
+        st.plotly_chart(fig_f1_macro_test)
+
+
+def dashboard_comments():
+    # --------------------------------------------------------
+    # Ejecutamos el codigo python para conectar con supabase
+    # --------------------------------------------------------
+    try:
+        # Spinner mientras consulta
+        with st.spinner("Consultando comments en Supabase..."):
+            dash_comments = pd.DataFrame(show_comments())
+
+        # Metricas principales
+        col1, col2, col3, col4 = st.columns(4)
+        with col1:
+            st.metric("Toxicos", f"{dash_comments['is_toxic'].sum()}", border=True)
+        with col2:
+            st.metric("No Toxicos", f"{(dash_comments['is_toxic'] == False).sum()}", border=True)
+        with col3:
+            st.metric("Número Comentarios", f"{len(dash_comments)}", border=True)
+        with col4:
+            st.metric("Media Toxicos", f"{dash_comments['is_toxic'].sum() / len(dash_comments) * 100:,.1f} %", border=True)
+
+
+        conteo = dash_comments['is_toxic'].value_counts(dropna=False).reset_index()
+        conteo.columns = ['Valor', 'Cantidad']
+
+        # Gráficos principales
+        col1, col2 = st.columns(2)
+        with col1:
+            # Crear el gráfico de barras con Plotly Express
+            fig_barras = px.bar(
+                conteo, 
+                x='Valor', 
+                y='Cantidad', 
+                title='Distribución de Valores de is_toxic',
+                color='Valor',
+                text_auto=True # Muestra el número exacto encima de cada barra
+            )
+            st.plotly_chart(fig_barras)
+        with col2:
+            # Crear el gráfico de circulo con Plotly Express
+            fig_circulo = px.pie(
+                conteo, 
+                names='Valor', 
+                values='Cantidad', 
+                title='Distribución de Valores de is_toxic',
+                color='Valor',
+                color_discrete_map={True: '#2CA02C', False: '#D62728'}
+            )
+            st.plotly_chart(fig_circulo)
+
+    except Exception as e:
+        st.error(f"Error conectando con Supabase: {e}")
+
+    # --------------------FIN conectar supabase-------------------------------------
 
 
 # -------------------------
-# Programa
+# Programa Principal
 # -------------------------
+
+# st.set_page_config(page_title="HateScan", page_icon="🤬", layout="wide")
+st.set_page_config(page_title="HateScan", page_icon="🤬")
 
 # Inyectamos el CSS en la aplicación
 st.markdown(f"<style>{css}</style>", unsafe_allow_html=True)
@@ -220,7 +371,7 @@ else:
                 # Si el usuario de Google no tiene una imagen de perfil, informamos de ello.
                 st.info("No hay imagen de perfil disponible.")
 
-        col1, col2 = st.columns(2)
+        col1, col2 = st.columns([4, 1])
         
         with col1:
             st.header(":orange[HateScan]")
@@ -229,10 +380,8 @@ else:
             st.header("🤬🤓")
 
     # -----------------------------------------------PESTAÑAS----------------------------------------------------
-    # Creamos las 3 pestañas
-    tab1, tab2, tab3 = st.tabs(["URL Video", "Comments", "Pestaña 3"])
-
-    df1 = pd.read_csv("data/raw/comments_dQw4w9WgXcQ_20260511.csv")
+    # Creamos las 4 pestañas
+    tab1, tab2, tab3, tab4, tab5 = st.tabs(["URL Video", "Comments", "Searches", "DashBoard Comments", "Dashboard Models"])
 
     with tab1:
         st.header("Datos del primer CSV")
@@ -257,41 +406,29 @@ else:
                     # ------------------------------------------
                     # Ejecutar scraping
                     with st.spinner("Extrayendo comentarios de YouTube..."):
-
-                        # # Clean the URL to obtain the 11-character video ID
-                        # video_id = extract_video_id(url_ingresada)
-                        
-                        # if not video_id:
-                        #     print(f"Skipping: Could not extract a valid video ID from {url_ingresada}")
-                        #     # continue
-                            
-                        # print(f"Extracted Video ID: {video_id}")
-                        
-                        # # Fetch the comments using the cleaned ID
-                        # comentarios = fetch_comments(video_id, max_results=5)
-
                         comentarios = get_comments(url_ingresada)
 
                     # Mostrar resultado
                     st.success("Comentarios obtenidos correctamente")
-                    # st.write(comentarios)
+                    st.write(comentarios)
 
-                    # Código para probar el JSON
-                    # st.write(resultado)
-                    # Convertimos la cadena JSON a un diccionario de Python
-                    datos = json.loads(resultado)
-                    st.write(datos)
-                    st.write(datos["model_used"])
-                    # st.write(datos["comments"])
-                    st.write(datos["comments"][0]["text_original"])
+                    # ----------------------------------------------------------------------------
+                    # Ejecutamos el codigo python para guardar los comments y searches en supabase
+                    # ----------------------------------------------------------------------------
+                    try:
+                        video_id = extract_video_id(url_ingresada)
+                    
+                        if not video_id:
+                            print(f"Skipping: Could not extract a valid video ID from {url_ingresada}")
 
-                    # Poner comentarios en formato dataframe
-                    df_comments = pd.DataFrame(datos["comments"])
-                    # Mostrar tabla
-                    st.dataframe(df_comments)
+                        # print(f"Extracted Video ID: {video_id}")
 
-                    # Recorrer los comentarios crear Json y actualizar Supabase con función
-                    st.write(save_comments(datos))
+                        save_hatescan_results(resultado, session_user, video_title, video_id)
+                        st.success("Datos insertados correctamente")
+
+                    except Exception as e:
+                        st.error(f"Error conectando con Supabase: {e}")
+                    # --------------------FIN guardar comments y searches en supabase-------------------------------------
                    
                     # Ejemplo: Mostrar la URL en un botón de enlace
                     st.link_button("Ir al video", st.session_state['ultima_url'])
@@ -302,38 +439,21 @@ else:
         st.divider()
         
     with tab2:
-        st.header("Datos del segundo CSV")
+        st.header("Comments")
 
-        boton_supabase = st.button("Supabase")
-        # if st.button("Supabase"):
-        if boton_supabase:
+        if st.button("Show_Comments"):
 
             # --------------------------------------------------------
             # Ejecutamos el codigo python para conectar con supabase
             # --------------------------------------------------------
             try:
                 # Spinner mientras consulta
-                with st.spinner("Consultando usuarios en Supabase..."):
+                with st.spinner("Consultando comments en Supabase..."):
+                    df_comments = pd.DataFrame(show_comments())
 
-                    # df_users = pd.DataFrame(get_users())
-                    # df_users = pd.DataFrame(search_user("juanmanuel.iriondo@gmail.com"))
-                    df_users = pd.DataFrame(search_user("juana@gmail.com"))
-                    # df_users = pd.DataFrame(search_user(st.user.email))
-
-                    # Comprobamos que ha devuelto algo
-                    if df_users.empty:
-                        st.write("El usuario No existe")
-                    else:
-                        st.write("El usuario YA existe")
-                        st.write(df_users["id"][0])
-
-                        dato = df_users["id"][0] + 1
-                        st.write(dato)
-
-                st.success("Usuarios obtenidos correctamente")
-
+                st.success("Comentarios obtenidos correctamente")
                 # Mostrar tabla
-                st.dataframe(df_users)
+                st.dataframe(df_comments)
 
             except Exception as e:
                 st.error(f"Error conectando con Supabase: {e}")
@@ -342,13 +462,49 @@ else:
 
         # Raya o separador al final de la pestaña
         st.divider()
-        
+
     with tab3:
-        st.header("Datos del tercer CSV")
-        st.dataframe(df1) # Muestra la tabla interactiva
+        st.header("Searches")
+
+        if st.button("Show_Searches"):
+
+            # --------------------------------------------------------
+            # Ejecutamos el codigo python para conectar con supabase
+            # --------------------------------------------------------
+            try:
+                # Spinner mientras consulta
+                with st.spinner("Consultando searches en Supabase..."):
+                    df_searches = pd.DataFrame(show_searches())
+
+                st.success("Busquedas obtenidas correctamente")
+                # Mostrar tabla
+                st.dataframe(df_searches)
+
+            except Exception as e:
+                st.error(f"Error conectando con Supabase: {e}")
+
+            # --------------------FIN conectar supabase-------------------------------------
+
+        # Raya o separador al final de la pestaña
+        st.divider()
+
+    with tab4:
+        dashboard_comments()
+
+        #Raya o separador al final de la pestaña
+        st.divider()
+        
+    with tab5:
+        dashboard_models()
 
         # Raya o separador al final de la pestaña
         st.divider()
     # -----------------------------------------------FIN DE PESTAÑAS----------------------------------------------
 
     st.button("Log out", on_click=st.logout)
+
+    # col21, col22 = st.columns([6, 1])
+    # with col21:
+    #     st.button("Log out", on_click=st.logout)
+    # with col22:
+    #      st.button("Créditos")
